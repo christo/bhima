6734a3c5da781c147d93eb853aec1cf96cb80bb8
Chris Mountford <chris@think.net.au>
2018-09-27 15:55:42 +1000

    tweak

--- dragonmind/mappings/fixmappings.pl
+++ dragonmind/mappings/fixmappings.pl
@@ -8,6 +8,8 @@ while (my $file = readdir(DIR)) {
         my $newname = "Mapping-$2-$3.png";
         if (not -f $newname) {
             print "should rename $file to $newname\n";
+            rename $file, $newname;
         }
     } 
+
 }
