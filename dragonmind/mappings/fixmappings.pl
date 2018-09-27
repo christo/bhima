23def09adc1bc014e2a81016c08445ab0bcaaa89
Chris Mountford <chris@think.net.au>
2018-09-27 14:48:22 +1000

    script to rename old mapping naming to fixed way

---
+++ dragonmind/mappings/fixmappings.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+opendir (DIR, ".") or die $!;
+while (my $file = readdir(DIR)) {
+    if ($file =~ /(Mapping(\d+)-\2-(.*)\.png)/) {
+        my $newname = "Mapping-$2-$3.png";
+        if (not -f $newname) {
+            print "should rename $file to $newname\n";
+        }
+    } 
+}
