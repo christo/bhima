2cb61612bd4bf40bdb89884058c5631afc306f39
Chris Mountford <chris@think.net.au>
2018-11-08 18:30:09 +1100

    added instructions for escaping from screen

--- connect-pp.sh
+++ connect-pp.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo attempting to launch screen to talk to pixelpusher on serial port
+echo to quit, send the sequence: ^a ^\\
 EXPECTED='/dev/tty.usbmodem12341'
 if [[ -c $EXPECTED ]]; then 
     screen $EXPECTED 115200
