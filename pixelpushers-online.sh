ba9ea9746251476fad5e4bb02563271117d3da1b
Chris Mountford <chris@think.net.au>
2020-04-24 15:08:28 +1000

    remove cruft

--- pixelpushers-online.sh
+++ pixelpushers-online.sh
@@ -2,11 +2,6 @@
 
 # reports on which pixel pushers are currently online using their known fixed IP addresses
 
-GREEN="\033[92m"
-YELLOW="\033[93m"
-RED="\033[91m"
-NORMAL="\033[0m"
-
 for pp in 1 2 3 4; do 
     ((ping -c 2 10.10.10.$pp 2>&1 >/dev/null && printf " \\033[92mPP${pp} online\\033[0m  ")  \
         || printf "\\033[91m PP${pp} offline\033[0m " ) &
