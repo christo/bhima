2bda48856e6d69109b22daa84c1b45c4999e516c
Chris Mountford <chris@think.net.au>
2020-04-27 14:56:36 +1000

    clarify

--- pixelpushers-online.sh
+++ pixelpushers-online.sh
@@ -2,9 +2,11 @@
 
 # reports on which pixel pushers are currently online using their known fixed IP addresses
 
-for pp in 1 2 3 4; do 
-    ((ping -c 2 10.10.10.$pp 2>&1 >/dev/null && printf " \\033[92mPP${pp} online\\033[0m  ")  \
-        || printf "\\033[91m PP${pp} offline\033[0m " ) &
+# print $1 in ansi $2
+p() { printf "\\033[$2m $1\033[0m "; }
+
+for pp in 1 2 3 4; do
+    ((ping -c 2 10.10.10.$pp 2>&1 >/dev/null && p "PP$pp online" 92 ) || p "PP$pp offline" 91 ) &
 done
 wait 
 echo
