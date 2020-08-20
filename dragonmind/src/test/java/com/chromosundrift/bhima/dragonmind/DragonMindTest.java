5ecfcca59e3a2abd29761f62bed86f465bca5dc6
Chris Mountford <chris@think.net.au>
2020-08-20 12:36:01 +1000

    format

--- dragonmind/src/test/java/com/chromosundrift/bhima/dragonmind/DragonMindTest.java
+++ dragonmind/src/test/java/com/chromosundrift/bhima/dragonmind/DragonMindTest.java
@@ -3,15 +3,10 @@ package com.chromosundrift.bhima.dragonmind;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-
-import static java.util.Arrays.stream;
-
 public class DragonMindTest {
     @Test
     public void testGetResourceUrl() {
         String url = DragonMind.getResourceFileOrUrl("dragon-logo.png");
         Assert.assertNotNull(url);
     }
-
 }
