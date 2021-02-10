6bba96a2cc7ae2669d87be204cc33026704b021d
Chris Mountford <chris@think.net.au>
2021-02-10 17:16:45 +1100

    upgraded gradle, added convenience script for testing build/deploy

---
+++ smoketest.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# builds distribution then unpacks and runs it locally, assumes we are connected to the dragon
+
+set -vx 
+shopt -s failglob
+
+#gradle clean
+gradle build
+mkdir -p tmp
+rm -rf tmp/*
+tar xf dragonmind/build/distributions/dragonmind-*.tar -C tmp/
+dd=tmp/dragonmind-*
+ln -s dragonmind/video ${dd#}/
+${dd#}/bin/dragonmind
