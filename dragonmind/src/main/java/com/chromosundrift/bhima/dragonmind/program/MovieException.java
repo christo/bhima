16b74c89120dffb3679c14ffeaa268cee5f697df
Chris Mountford <chris@think.net.au>
2021-02-20 00:19:39 +1100

    added MovieException class

---
+++ dragonmind/src/main/java/com/chromosundrift/bhima/dragonmind/program/MovieException.java
@@ -0,0 +1,7 @@
+package com.chromosundrift.bhima.dragonmind.program;
+
+public class MovieException extends Exception {
+    public MovieException(String message) {
+        super(message);
+    }
+}
