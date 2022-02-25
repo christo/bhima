72e551b86e395a0e1aa72c448825c06150e46cca
Chris Mountford <chris@think.net.au>
2022-02-25 12:23:54 +1100

    making script a little cleaner

--- tiger-sync.sh
+++ tiger-sync.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
+# syncs project dir and all local java repos to tiger server or given target machine
+# needed because we may need to rebuild on the deployment target at a festival without
+# internet access. NOTE: expects ssh config for target host to define username. Works
+# best with pubkey auth, otherwise multiple password prompts will occur
+# target machine is assumed to use src/christo as location of project dir
+
+# usage: tiger-sync.sh [target_host]
+
 export TIGER=${1:-tiger.local}
 
-rsync -vazu --delete-after /Users/christo/src/christo/bhima christo@$TIGER:src/christo
-rsync -vazu /Users/christo/src/christo/processing christo@$TIGER:src/christo
-rsync -vazu /Users/christo/.ivy2 christo@$TIGER:
-rsync -vazu /Users/christo/.gradle christo@$TIGER:
-rsync -vazu /Users/christo/.m2 christo@$TIGER:
+export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# TODO rely on externally defined $PROCESSING_SKETCH_DIR
+export PROCESSING_SKETCH_DIR=$HOME/christo/processing
+
+# TODO update this when moving this scripts/
+rsync -vazu --delete-after "$SCRIPT_DIR" $TIGER:src/christo
+rsync -vazu "$PROCESSING_SKETCH_DIR" $TIGER:src/christo
+rsync -vazu $HOME/.ivy2 $TIGER:
+rsync -vazu $HOME/.gradle $TIGER:
+rsync -vazu $HOME/.m2 $TIGER:
