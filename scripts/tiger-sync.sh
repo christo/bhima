2ea5ea4f4f5964f385c646db0ceae5acb3a0a5a0
Chris Mountford <chris@think.net.au>
2022-03-14 20:06:59 +1100

    move sync script to scripts dir, delete cruft

--- scripts/tiger-sync.sh
+++ scripts/tiger-sync.sh
@@ -12,11 +12,12 @@ export TIGER=${1:-tiger.local}
 
 # TODO move this file to scripts/ and update
 export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export PROJECT_DIR="$SCRIPT_DIR/.."
 
 # TODO rely on externally defined $PROCESSING_SKETCH_DIR
 export PROCESSING_SKETCH_DIR=$HOME/src/christo/processing
 
-rsync -vazu --delete-after "$SCRIPT_DIR" $TIGER:src/christo
+rsync -vazu --delete-after "$PROJECT_DIR" $TIGER:src/christo
 rsync -vazu "$PROCESSING_SKETCH_DIR" $TIGER:src/christo
 rsync -vazu $HOME/.ivy2 $TIGER:
 rsync -vazu $HOME/.gradle $TIGER:
--- tiger-sync-reverse.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO move to scripts/
-
-rsync -vazu --delete-after christo@10.10.10.101:src/christo/bhima /Users/christo/src/christo/bhima-tiger/
-#rsync -vazu /Users/christo/src/christo/processing christo@10.10.10.101:src/christo
-#rsync -vazu christo@10.10.10.101:.ivy2 /Users/christo/.ivy2 
-#rsync -vazu christo@10.10.10.101:.gradle /Users/christo/.gradle 
-#rsync -vazu christo@10.10.10.101:.m2 /Users/christo/.m2 
