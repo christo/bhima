240027d0b7c49ce52030bdb095a011907006b22e
Chris Mountford <chris@think.net.au>
2022-03-14 21:16:46 +1100

    make sure processing dir is a clean sync

--- scripts/tiger-sync.sh
+++ scripts/tiger-sync.sh
@@ -16,7 +16,7 @@ export PROJECT_DIR=$(dirname "$SCRIPT_DIR")
 export PROCESSING_SKETCH_DIR=$HOME/src/christo/processing
 
 rsync -vazu --delete-after "$PROJECT_DIR" $TIGER:src/christo
-rsync -vazu "$PROCESSING_SKETCH_DIR" $TIGER:src/christo
+rsync -vazu --delete-after "$PROCESSING_SKETCH_DIR" $TIGER:src/christo
 rsync -vazu $HOME/.ivy2 $TIGER:
 rsync -vazu $HOME/.gradle $TIGER:
 rsync -vazu $HOME/.m2 $TIGER:
